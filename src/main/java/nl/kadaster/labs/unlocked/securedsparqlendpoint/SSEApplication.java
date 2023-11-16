5881d100fc0b0c50645d9f7af9c30aba532154d5
Sven Mol <s.mol@student.utwente.nl>
2023-11-16 14:40:28 +0100

    Initialize Spring project

---
+++ src/main/java/nl/kadaster/labs/unlocked/securedsparqlendpoint/SSEApplication.java
@@ -0,0 +1,11 @@
+package nl.kadaster.labs.unlocked.securedsparqlendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSEApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SSEApplication.class, args);
+    }
+}
---
+++ src/test/java/nl/kadaster/labs/unlocked/securedsparqlendpoint/SSEApplicationTest.java
@@ -0,0 +1,11 @@
+package nl.kadaster.labs.unlocked.securedsparqlendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSEApplicationTest {
+	@Test
+	void contextLoads() {
+	}
+}
