e80a86ba28eeb3e542648c19aacadc92d134a21d
Sven Mol <s.mol@student.utwente.nl>
2024-02-20 09:37:09 +0100

    Allow specifying datasets via command line

--- src/main/java/nl/kadaster/labs/unlocked/securedsparqlendpoint/repositories/DatasetRepository.java
+++ src/main/java/nl/kadaster/labs/unlocked/securedsparqlendpoint/repositories/DatasetRepository.java
@@ -1,16 +1,25 @@
 package nl.kadaster.labs.unlocked.securedsparqlendpoint.repositories;
 
+import lombok.extern.slf4j.Slf4j;
 import nl.kadaster.labs.unlocked.securedsparqlendpoint.Dataset;
 import nl.kadaster.labs.unlocked.securedsparqlendpoint.util.GlobUtil;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
 @Component
+@Slf4j
 public class DatasetRepository extends Repository<Dataset> {
-    public DatasetRepository() throws IOException {
-        for (Path path : GlobUtil.findAll(Path.of("./data/"), "*")) {
+    public DatasetRepository(ApplicationArguments appArgs) throws IOException {
+        var args = appArgs.getSourceArgs();
+        var file = args.length == 0 ? "*" : args[0];
+        var paths = GlobUtil.findAll(Path.of("./data/"), file);
+
+        log.info("Loading {} {}", paths.size(), paths.size() == 1 ? "ontology" : "ontologies");
+
+        for (Path path : paths) {
             if (path.toString().contains(Dataset.AUTHORISATION_ONTOLOGY_INFIX)) continue;
             var dataset = new Dataset(path);
             this.entries.put(dataset.name, dataset);
